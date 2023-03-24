8faaf83db8b19c50f21da873c1e82481614609e8
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-03-24 06:13:59 -0400

    Basic Tile-set and map with collisions.

--- characters/player_cat.gd
+++ characters/player_cat.gd
@@ -19,7 +19,7 @@ func _physics_process(_delta) -> void:
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	print(input_direction)
+	# print(input_direction)
 	
 	update_animation_parameters(input_direction)
 	velocity = input_direction * move_speed
