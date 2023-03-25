c2e36b9c981e936f92a71348dc2f5e8f053e48a9
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-03-24 21:53:15 -0400

    Refactored player.gd
    Refactored player.gd for increased readability.

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,22 +8,31 @@ class_name Player
 @onready var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 
 func _ready() -> void:
-	animation_tree.set("parameters/Idle/blend_position", starting_direction)
+	set_idle_blend_position(starting_direction)
 
-func _physics_process(_delta) -> void:
-	var input_direction = Vector2(
-		Input.get_axis("left", "right"),
-		Input.get_axis("up", "down")
-	)
+func _physics_process(_delta: float) -> void:
+	var input_direction = get_input_direction()
 	update_animation_parameters(input_direction)
 	velocity = input_direction * move_speed
 	move_and_slide()
 	pick_new_state()
 	
+func get_input_direction() -> Vector2:
+	return Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	)
+
 func update_animation_parameters(move_input: Vector2) -> void:
 	if move_input != Vector2.ZERO:
-		animation_tree.set("parameters/Walk/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
+		set_walk_blend_position(move_input)
+		set_idle_blend_position(move_input)
+
+func set_idle_blend_position(position: Vector2) -> void:
+	animation_tree.set("parameters/Idle/blend_position", position)
+
+func set_walk_blend_position(position: Vector2) -> void:
+	animation_tree.set("parameters/Walk/blend_position", position)
 		
 func pick_new_state() -> void:
 	if velocity != Vector2.ZERO:
