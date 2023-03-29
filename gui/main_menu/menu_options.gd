7423891eafe60d4bd0b9904403b989cce0a90447
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-03-29 11:28:53 -0400

    Connected main menu button signals via code
    Unconnected the button signals via editor and added it to a function.

--- gui/main_menu/menu_options.gd
+++ gui/main_menu/menu_options.gd
@@ -3,17 +3,29 @@ extends VFlowContainer
 @export var gameplay_scene : PackedScene
 @export var settings_scene : PackedScene
 
+@onready var start_btn : Button = $Start
+@onready var settings_btn : Button = $Settings
+@onready var quit_btn : Button = $Quit
+
+
 func _ready() -> void:
 	get_children()[0].grab_focus()
-	
+	_connect_signals()
 	if !OS.has_feature("pc"):
 		$Quit.hide()
 
-func _on_quit_pressed():
+
+func _connect_signals() -> void:
+	start_btn.connect("pressed", _on_start_pressed)
+	settings_btn.connect("pressed", _on_settings_pressed)
+	quit_btn.connect("pressed", _on_quit_pressed)
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-func _on_settings_pressed():
+func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_packed(settings_scene)
 
-func _on_start_pressed():
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(gameplay_scene)
+
