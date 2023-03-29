035a608865adfaf7ae4c080d4cc928a78ab0e4dd
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-03-29 15:14:05 -0400

    Restructured folders
    Moved drawing.tscn to its own folder under res:///gui/
    Added some static typing to drawing.gd

--- gui/drawing/drawing.gd
+++ gui/drawing/drawing.gd
@@ -3,14 +3,14 @@ extends Control
 @export var line_width = 10
 @export var line_color = Color(1, 1, 1, 1)
 
-@onready var line : Line2D = $Line
+@onready var line : Line2D = %Line
 @onready var texture_rect : ReferenceRect = %ReferenceRect
 
-func _ready():
+func _ready() -> void:
 	line.width = line_width
 	line.default_color = line_color
 
-func _input(event: InputEvent):
+func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			var local_position = _get_local_position(event.position)
@@ -22,12 +22,12 @@ func _input(event: InputEvent):
 					emit_signal("drawing_complete", line.get_points())
 
 	elif event is InputEventMouseMotion:
-		if event.get_button_mask() & MOUSE_BUTTON_MASK_LEFT:
+		if event.get_button_mask() and MOUSE_BUTTON_MASK_LEFT:
 			var local_position = _get_local_position(event.position)
 			if texture_rect.get_rect().has_point(local_position):
 				line.add_point(local_position)
 
-func _get_local_position(event_position) -> Vector2:
+func _get_local_position(event_position: Vector2) -> Vector2:
 	var local_position = get_viewport().get_canvas_transform().affine_inverse().basis_xform(event_position)
 	local_position -= get_global_position()
 	return local_position
