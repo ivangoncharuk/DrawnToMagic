4e7b519bb7589a2de7b2e56ee489ae984681ece3
Ezekiel <ehalle4@students.towson.edu>
2023-04-24 23:45:39 -0400

    Updated demo

---
+++ scripts/gesture_recognizer/demo/demo.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var gesture_recognizer := GestureRecognizer.new()
+@onready var draw_surface = $DrawSurface
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_pressed() and event.keycode == KEY_ENTER:
+			var t = gesture_recognizer.recognize(draw_surface.create_gesture())
+			draw_surface.clear()
+			print(t.name)
+
+
+func _ready() -> void:
+	var gesture := Gesture.new()
+	var plus_glyph: Array[Point] = []
+	var line_glyph: Array[Point] = []
+	var step := .05
+	var length := 10000
+
+	# Constructs horizontal line
+	for i in length:
+		plus_glyph.append(Point.new(Vector2(i * step, .5), 0))
+	
+	# Construct vertical line
+	for i in length:
+		plus_glyph.append(Point.new(Vector2(length / 2.0, i * step), 1))
+	
+	for i in length:
+		line_glyph.append(Point.new(Vector2(i * step, .5), 0))
+
+	gesture.name = "Plus"
+	gesture.initialize(line_glyph)
+	gesture_recognizer.templates.append(gesture)
+	
+	gesture = Gesture.new()
+	gesture.name = "Line"
+	gesture.initialize(plus_glyph)
+	gesture_recognizer.templates.append(gesture)
--- scripts/gesture_recognizer/draw_surface.gd
+++ scripts/gesture_recognizer/draw_surface.gd
@@ -3,41 +3,51 @@ extends Control
 var _raw_points: Array[Point] = []
 var _current_stroke_id := 0
 var _is_draw_started := false
+var _color_by_id := {0:Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255))}
 
 func _gui_input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        if event.pressure >= 0.1:
-            _is_draw_started = true
-            _raw_points.append(Point.new(event.position, _current_stroke_id, event.pressure))
-            queue_redraw()
-        elif _is_draw_started:
-            _is_draw_started = false
-            _current_stroke_id += 1
-    if event is InputEventMouseButton:
-        if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-            pass
+	if event is InputEventMouseMotion:
+		if event.pressure >= 0.1:
+			_is_draw_started = true
+			_raw_points.append(Point.new(event.position, _current_stroke_id, event.pressure))
+			queue_redraw()
+		elif _is_draw_started:
+			_is_draw_started = false
+			_current_stroke_id += 1
+			_color_by_id[_current_stroke_id] = Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255))
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			pass
 
 
 func _draw() -> void:
-    if _raw_points.is_empty():
-        return
-    
-    var first_point: Point = _raw_points.front()
-    draw_circle(first_point.position, 1.5, Color.WHITE)
+	if _raw_points.is_empty():
+		return
+	
+	var first_point: Point = _raw_points.front()
+	var color := Color8(randi_range(0, 255), randi_range(0, 255), randi_range(0, 255))
+	draw_circle(first_point.position, 1.5 * first_point.pressure, color)
 
-    for i in range(1, _raw_points.size()):
-        var current_point := _raw_points[i]
-        var previous_point := _raw_points[i - 1]
+	for i in range(1, _raw_points.size()):
+		
+		var current_point := _raw_points[i]
+		var previous_point := _raw_points[i - 1]
 
-        if previous_point.stroke_id == current_point.stroke_id:
-            draw_line(previous_point.position, current_point.position, Color.WHITE, 2 * current_point.pressure)
-        else:
-            draw_circle(current_point.position,  2 * current_point.pressure, Color.WHITE)
+		if previous_point.stroke_id == current_point.stroke_id:
+			draw_line(previous_point.position, current_point.position, color, 2 * current_point.pressure)
+		else:
+			color = _color_by_id[current_point.stroke_id]
+			draw_circle(current_point.position,  2 * current_point.pressure, color)
 
 
 func create_gesture() -> Gesture:
-    var gesture := Gesture.new()
-    gesture.initialize(_raw_points)
-    return gesture
+	var gesture := Gesture.new()
+	gesture.initialize(_raw_points)
+	return gesture
 
 
+func clear() -> void:
+	_raw_points.clear()
+	_current_stroke_id = 0
+	_is_draw_started = false
+	queue_redraw.call_deferred()
